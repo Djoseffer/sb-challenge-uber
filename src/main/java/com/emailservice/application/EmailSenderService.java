a37ecc33592103a83e7a7d2264fa81d79d34e099
Djoseffer <tom_djosefer@hotmail.com>
2024-05-12 20:33:48 -0300

    feat: services created

---
+++ src/main/java/com/emailservice/application/EmailSenderService.java
@@ -0,0 +1,24 @@
+package com.emailservice.application;
+
+import com.emailservice.adapters.EmailSenderGateway;
+import com.emailservice.core.EmailSenderUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class EmailSenderService implements EmailSenderUseCase {
+
+    private final EmailSenderGateway emailSenderGateway;
+
+    @Autowired
+    public EmailSenderService(EmailSenderGateway emailGateway){
+        this.emailSenderGateway= emailGateway;
+    }
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        this.emailSenderGateway.sendEmail(to, subject, body);
+    }
+}
